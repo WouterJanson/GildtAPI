d46b303f01a10e2bc81160b5fcaa6abec5c85228
Menno de Jonge <mennodejonge94@gmail.com>
2018-11-05 20:12:39 +0100

    Added password hash

---
+++ PasswordHasher.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Web;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+
+public static class PasswordHasher
+    {
+        private const int HashSize = 20;
+        private const int SaltSize = 16;
+        private const int HashBytesSize = SaltSize + HashSize;
+        private const int NumberOfIterations = 2000; 
+        public static string HashPassword(string password)
+        {
+            if(string.IsNullOrEmpty(password))
+            {
+                return null; 
+            }
+
+            byte[] salt = new byte[SaltSize];
+            new RNGCryptoServiceProvider().GetBytes(salt);
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, NumberOfIterations);
+            byte[] hash = pbkdf2.GetBytes(HashSize);
+            byte[] hashBytes = new byte[HashBytesSize];
+
+            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(hash, 0, hashBytes, 16, HashSize);
+
+            string hashedPassword = Convert.ToBase64String(hashBytes);
+            return hashedPassword; 
+        }
+
+        // Test if the passworh hash is correctly
+        public static bool VerifyHashedPassword(string hashedPassword, string unhashedPassword)
+        {
+            if(string.IsNullOrEmpty(hashedPassword))
+            {
+                return false; 
+            }
+            byte[] hashBytes = Convert.FromBase64String(hashedPassword);
+            byte[] salt = new byte[SaltSize];
+            Array.Copy(hashBytes, 0, salt, 0, SaltSize);
+            var pbkdf2 = new Rfc2898DeriveBytes(unhashedPassword, salt, NumberOfIterations);
+            byte[] hash = pbkdf2.GetBytes(HashSize);
+
+            for (int i = 0; i < HashSize; i++)
+            {
+                if(hashBytes[i+SaltSize] != hash[i])
+                {
+                    return false; 
+                }
+            }
+
+            return true; 
+        }
+    }
