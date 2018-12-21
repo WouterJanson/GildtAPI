7dbcecce930628a3326f480aecf8bbce74b4dbf9
Menno de Jonge <mennodejonge94@gmail.com>
2018-12-21 18:42:01 +0100

    Added Singleton

---
+++ Singleton.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildtAPI {
+	class Singleton<T> where T : new(){
+		private static T instance;
+
+		public static T Instance {
+			get {
+				if (instance == null) {
+					instance = new T();
+				}
+
+				return instance;
+			}
+		}
+	}
+}
